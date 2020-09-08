aa64ca9f054f51e0edaa37d040caf2300a490d38
subashlimbu <prajan_l@hotmail.com>
2020-09-08 23:40:32 +0100

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding with redux flow for directory/shop

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -34,7 +34,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${total}</span>
+      <span>TOTAL: £{total}</span>
     </div>
   </div>
 );
